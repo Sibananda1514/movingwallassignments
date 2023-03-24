8d514576180fb652f9daab17a5e9aef0cb378d3c
Sibananda.Acharjya <sibananda.acharjya@mindfiresolutions.com>
2023-03-24 12:16:34 +0530

    Remove credntial from properties file

--- src/main/java/com/mindfire/constants/ResponseConstant.java
+++ src/main/java/com/mindfire/constants/ResponseConstant.java
@@ -6,8 +6,8 @@ public class ResponseConstant {
 		
 	}
 	
-	public final static String ISSUE_CREATE="Issue create succesfully";
-	public final static String ISSUE_DELETE="Issue delet succesfully";
-	public final static String ISSUE_EDIT="Issue edit succesfully";
-	public final static String ISSUE_LIST="Requeste issue listed succesfully";
+	public final static String ISSUE_CREATE="The issue has been created successfully.";
+	public final static String ISSUE_DELETE="The issue has been deleted successfully.";
+	public final static String ISSUE_EDIT="The issue has been edited successfully.";
+	public final static String ISSUE_LIST="The requested issue has been listed successfully.";
 }
